4772aae72ce9022fd889b07f2b260317dbcfc16f
dasdeo <dasdeo@proton.me>
2023-12-05 07:40:00 +0100

    Update ci/cd workflow.

--- .cicd/deploy.sh
+++ .cicd/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -eufx
 
-npm i
+printf "Deploying with version '%s'\n" "$RELEASE_VERSION"
 
 rm -f ./dist/tardp.mjs
 
@@ -12,4 +12,5 @@ curl \
 	-H "Content-Type:application/octet-stream" \
 	-H "x-anio-auth-key: $ANIO_SH_DEPLOY_KEY" \
 	-H "x-anio-file-name: tardp" \
+	-H "x-anio-release-version: $RELEASE_VERSION" \
 	"$ANIO_SH_DEPLOY_URL"
